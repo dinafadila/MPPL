c02e50a3640c50960615cff0434ff16dd204d3da
dina <dinafadila@users.noreply.github.com>
2020-12-16 11:24:43 +0700

    calendar user, fixing routes

--- resources/views/pages/calendar.blade.php
+++ resources/views/pages/calendar.blade.php
@@ -3,22 +3,52 @@
 @section('content')
 <div class="content">
   <div class="container-fluid">
-    <div class="card">
-      <div class="card-header card-header-success">
-        <h3 class="card-title">Kalender Akademik</h3>
-        <p class="card-category">Semester Ganjil</p>
-      </div>
-      <div class="card-body">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header card-header-success">
+            <h4 class="card-title ">Kalender Akademik</h4>
+            <p class="card-category">Semester Ganjil</p>
+          </div>
+          <div class="card-body">
             <div class="table-responsive">
               <table class="table">
                 <thead class=" text-primary">
-               
+                  <th style="width: 1%">
+                    No
+                  </th>
+                  <th>
+                    Nama Kegiatan
+                  </th>
+                  <th>
+                    Tanggal mulai
+                  </th>
+                  <th>
+                    Tanggal berakhir
+                  </th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      1
+                    </td>
+                    <td>
+                      Hari Libur Nasional
+                    </td>
+                    <td>
+                      10-07-2020
+                    </td>
+                    <td>
+                      13-07-2020
+                    </td>
+                    
                   </tr>
                 </tbody>
               </table>
             </div>
           </div>
         </div>
+      </div>
       </div>
     </div>
   </div>
