383ff8afca824c506519c393661f02362d2b4764
afyuda6 <yuda_p411@apps.ipb.ac.id>
2020-12-17 07:45:19 +0700

    fixing minor

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -7,22 +7,7 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="{{ route('admin') }}" class="nav-link">Beranda</a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Kontak</a>
-      </li>
     </ul>
-
-    <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
-      <div class="input-group input-group-sm">
-        <input class="form-control form-control-navbar" type="search" placeholder="Cari" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-navbar" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-      </div>
-    </form>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
